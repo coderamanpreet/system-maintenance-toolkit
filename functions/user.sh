dca02ebce11cfd2c2f05d34c17cad717ad421cb6
Amanpreet <156164040+coderamanpreet@users.noreply.github.com>
2025-05-14 10:55:18 +0530

    Update user.sh

--- functions/user.sh
+++ functions/user.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 user_manager() {
+
     OPERATION=$(whiptail --title "User Manager" --menu "Choose operation:" 15 50 3 \
         "1" "Add a User" \
         "2" "Delete a User" \
@@ -14,7 +15,12 @@ user_manager() {
                 whiptail --msgbox "User $USERNAME already exists!" 10 50
             else
                 sudo useradd -m "$USERNAME"
-                whiptail --msgbox "User $USERNAME added." 10 50
+                # Prompt to set a password for the new user
+                PASSWORD=$(whiptail --passwordbox "Enter password for $USERNAME:" 10 50 3>&1 1>&2 2>&3)
+                echo "$USERNAME:$PASSWORD" | sudo chpasswd
+                whiptail --msgbox "User $USERNAME added and password set." 10 50
+                NEW_USERS=$(awk -F: '$3>=1000 && $3!=65534 {print $1}' /etc/passwd)
+                whiptail --msgbox "User $USERNAME created successfully!\n\nUpdated list of users:\n$NEW_USERS" 20 70
                 echo "[Added User: $USERNAME] $(date)" >> "$1"
             fi
             ;;
@@ -23,6 +29,8 @@ user_manager() {
             if id "$USERNAME" &>/dev/null; then
                 sudo userdel -r "$USERNAME"
                 whiptail --msgbox "User $USERNAME deleted." 10 50
+                UPDATED_USERS=$(awk -F: '$3>=1000 && $3!=65534 {print $1}' /etc/passwd)
+                whiptail --msgbox "Updated list of users after deletion:\n$UPDATED_USERS" 20 70
                 echo "[Deleted User: $USERNAME] $(date)" >> "$1"
             else
                 whiptail --msgbox "User $USERNAME does not exist!" 10 50
@@ -35,3 +43,5 @@ user_manager() {
             ;;
     esac
 }
+
+
